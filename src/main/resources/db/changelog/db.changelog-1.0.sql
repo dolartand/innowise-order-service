0b94ddacd17ee36af82ce77dbec54e1a0e5019b4
dolartand <dolartand@gmail.com>
2025-11-21 21:12:52 +0300

    feat: add db schema with migrations

---
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,39 @@
+-- liquibase formatted sql
+
+-- changeset dolartand:1
+CREATE TABLE items (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL CHECK ( price >= 0 ),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
+-- changeset dolartand:2
+CREATE TABLE orders (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    status VARCHAR(50) NOT NULL DEFAULT 'PENDING',
+    total_price DECIMAL(10, 2) NOT NULL DEFAULT 0.00 CHECK ( total_price >= 0 ),
+    deleted BOOLEAN NOT NULL DEFAULT false,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+)
+
+-- changeset dolartand:3
+CREATE TABLE order_items(
+    id BIGSERIAL PRIMARY KEY,
+    order_id BIGINT NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    item_id BIGINT NOT NULL REFERENCES items(id) ON DELETE RESTRICT,
+    quantity INT NOT NULL CHECK ( quantity > 0 ),
+    price DECIMAL(10, 2) NOT NULL CHECK (price >= 0),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+)
+
+-- changeset dolartand:4
+CREATE INDEX idx_orders_user_id ON orders(user_id);
+CREATE INDEX idx_orders_status ON orders(status);
+CREATE INDEX idx_order_items_order_id ON order_items(order_id);
+CREATE INDEX idx_order_items_item_id ON order_items(item_id);
+CREATE INDEX idx_items_name ON items(name);
